17b795cc1ecff15faad5a9b8c3bff7003e6d1787
Aram Khachatryan <khachatryan.a707@gmail.com>
2023-08-16 11:04:48 -0700

    wip

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,23 +1,48 @@
-import React from 'react'
-import axios from 'axios'
-import Character from './Character'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Character from './Character';
 
-const urlPlanets = 'http://localhost:9009/api/planets'
-const urlPeople = 'http://localhost:9009/api/people'
+const urlPlanets = 'http://localhost:9009/api/planets';
+const urlPeople = 'http://localhost:9009/api/people';
 
 function App() {
-  // ❗ Create state to hold the data from the API
-  // ❗ Create effects to fetch the data and put it in state
+  const [characters, setCharacters] = useState([]);
+  const [planets, setPlanets] = useState([]);
+
+  useEffect(() => {
+    
+    axios.get(urlPeople)
+      .then(response => {
+        setCharacters(response.data);
+      })
+      .catch(error => {
+        console.log('Error fetching characters:', error);
+      });
+
+    axios.get(urlPlanets)
+      .then(response => {
+        setPlanets(response.data);
+      })
+      .catch(error => {
+        console.log('Error fetching planets:', error);
+      });
+  }, []);
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
-      <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {characters.map(character => {
+        const characterPlanet = planets.find(planet => planet.id === character.homeworld);
+
+        return (
+          <Character key={character.id} character={character} planet={characterPlanet} />
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
 
 // ❗ DO NOT CHANGE THE CODE  BELOW
 if (typeof module !== 'undefined' && module.exports) module.exports = App
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function Character() { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  // ❗ Create a "toggle" click handler to show or remove the homeworld
+const Character = ({ character, planet }) => {
+  const [showPlanet, setShowPlanet] = useState(false);
+
+  const togglePlanet = () => {
+    setShowPlanet(prevShowPlanet => !prevShowPlanet);
+  };
+
+  
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
+    <div className="character-card" onClick={togglePlanet}>
+      <div className="character-name">{character.name}</div>
+      {showPlanet && (
+        <div className="character-planet">
+          {planet ? `Homeworld: ${planet.name}` : 'Planet information not available'}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Character
+export default Character;
